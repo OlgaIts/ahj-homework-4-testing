735d2cb28b68de36e41d606c32f5b1a2f6eb0f14
Olga Its <secirija33@gmail.com>
2024-08-23 14:54:41 +0200

    fix yml file

--- webpack.config.js
+++ webpack.config.js
@@ -10,143 +10,142 @@ const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
 
 const optimization = () => {
-  const config = {
-    splitChunks: {
-      chunks: 'all',
+ const config = {
+  splitChunks: {
+   chunks: 'all',
+  },
+ }
+
+ if (isProd) {
+  config.minimizer = [
+   new TerserWebpackPlugin({
+    terserOptions: {
+     format: {
+      comments: false,
+     },
     },
-  }
-
-  if (isProd) {
-    config.minimizer = [
-      new TerserWebpackPlugin({
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-      new CssMinimizerWebpackPlugin({
-        minimizerOptions: {
-          preset: [
-            'default',
-            {
-              discardComments: { removeAll: true },
+    extractComments: false,
+   }),
+   new CssMinimizerWebpackPlugin({
+    minimizerOptions: {
+     preset: [
+      'default',
+      {
+       discardComments: { removeAll: true },
+      },
+     ],
+    },
+   }),
+   new ImageMinimizerPlugin({
+    minimizer: {
+     implementation: ImageMinimizerPlugin.imageminMinify,
+     options: {
+      plugins: [
+       ['gifsicle', { interlaced: true }],
+       ['jpegtran', { progressive: true }],
+       ['optipng', { optimizationLevel: 5 }],
+       [
+        'svgo',
+        {
+         plugins: [
+          {
+           name: 'preset-default',
+           params: {
+            overrides: {
+             removeViewBox: false,
+             addAttributesToSVGElement: {
+              params: {
+               attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+              },
+             },
             },
-          ],
-        },
-      }),
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            plugins: [
-              ['gifsicle', { interlaced: true }],
-              ['jpegtran', { progressive: true }],
-              ['optipng', { optimizationLevel: 5 }],
-              [
-                'svgo',
-                {
-                  plugins: [
-                    {
-                      name: 'preset-default',
-                      params: {
-                        overrides: {
-                          removeViewBox: false,
-                          addAttributesToSVGElement: {
-                            params: {
-                              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-                            },
-                          },
-                        },
-                      },
-                    },
-                  ],
-                },
-              ],
-            ],
+           },
           },
+         ],
         },
-      }),
-    ]
-  }
+       ],
+      ],
+     },
+    },
+   }),
+  ]
+ }
 
-  return config
+ return config
 }
 
 module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  target: 'web',
-  devServer: {
-    port: 9000,
-    open: true,
-    hot: true,
-  },
-  entry: path.resolve(basePath, 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    filename: '[name].[contenthash:10].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash:10].css',
-    }),
-  ],
-  optimization: optimization(),
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
+ mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+ target: 'web',
+ devServer: {
+  open: true,
+  hot: true,
+ },
+ entry: path.resolve(basePath, 'index.js'),
+ output: {
+  path: path.resolve(__dirname, 'dist'),
+  clean: true,
+  filename: '[name].[contenthash:10].js',
+ },
+ plugins: [
+  new HtmlWebpackPlugin({
+   template: './src/index.html',
+  }),
+  new MiniCssExtractPlugin({
+   filename: '[name].[contenthash:10].css',
+  }),
+ ],
+ optimization: optimization(),
+ module: {
+  rules: [
+   {
+    test: /\.html$/,
+    loader: 'html-loader',
+   },
+   {
+    test: /\.(woff(2)?|eot|ttf|otf|)$/i,
+    exclude: /node_modules/,
+    type: 'asset/resource',
+    generator: {
+     filename: path.join('fonts', '[name].[contenthash:10][ext]'),
+    },
+   },
+   {
+    test: /\.ico$/i,
+    type: 'asset/resource',
+    generator: {
+     filename: path.join('img', '[name].[contenthash:10][ext]'),
+    },
+   },
+   {
+    test: /\.(png|jpe?g|gif|svg|webp)$/i,
+    exclude: /node_modules/,
+    type: 'asset/resource',
+    generator: {
+     filename: path.join('img', '[name].[contenthash:10][ext]'),
+    },
+   },
+   {
+    test: /\.s?css$/i,
+    exclude: /node_modules/,
+    use: [
+     'style-loader',
+     {
+      loader: 'css-loader',
+      options: {
+       modules: {
+        localIdentName: '[name]__[local]--[hash:base64:5]',
+        auto: (resPath) => !!resPath.includes('.module.'),
+       },
       },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|)$/i,
-        exclude: /node_modules/,
-        type: 'asset/resource',
-        generator: {
-          filename: path.join('fonts', '[name].[contenthash:10][ext]'),
-        },
-      },
-      {
-        test: /\.ico$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: path.join('img', '[name].[contenthash:10][ext]'),
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        exclude: /node_modules/,
-        type: 'asset/resource', 
-        generator: {
-          filename: path.join('img', '[name].[contenthash:10][ext]'),
-        },
-      },
-      {
-        test: /\.s?css$/i,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]--[hash:base64:5]',
-                auto: (resPath) => !!resPath.includes('.module.'),
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
+     },
     ],
-  },
+   },
+   {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader',
+   },
+  ],
+ },
 }
